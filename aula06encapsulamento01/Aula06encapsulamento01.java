8c57491451c7ca627a40d8224f9cdbafa8ed0b79
HamiltonStefani <88253849+HamiltonStefani@users.noreply.github.com>
2023-01-03 18:59:24 -0300

    Add files via upload

---
+++ aula06encapsulamento01/Aula06encapsulamento01.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package aula06encapsulamento01;
+
+/**
+ *
+ * @author hamil
+ */
+public class Aula06encapsulamento01 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        ControleRemoto  c = new ControleRemoto();
+        
+        c.maisVolume();
+        c.play();
+        c.abrirMenu();
+        c.fecharMenu();
+    }
+    
+}
